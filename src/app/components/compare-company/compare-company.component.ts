b55d44d958a0944dbc7157b41ee394974f3fc905
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2019-12-10 14:51:57 +0800

    commit compare company function

--- src/app/components/compare-company/compare-company.component.ts
+++ src/app/components/compare-company/compare-company.component.ts
@@ -7,79 +7,193 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./compare-company.component.scss']
 })
 export class CompareCompanyComponent implements OnInit {
-
+    
     rangeValue: number = 60;
     // form part
-    companyName: string;
+    paramOfFirstChart: string;
     startTime: string;
     endTime: string;
-    anyList: Object;
-    arraytemp: [];
-    arraytempStr: string;
-    companySinglePriceData: Object;
-    companySinglePriceType: string;
-    companySinglePriceFusion : Object;
-    companySinglePriceFusion_data : Object;
-  constructor(private http: HttpClient) {
-    //数据部分
-    this.companyName = "500112";
-    this.companySinglePriceType = "line";
-    this.companySinglePriceFusion_data = [{
-        "label": "Mon",
-        "value": "15123"
-    },
-    {
-        "label": "Tue",
-        "value": "14233"
-    },
-    {
-        "label": "Wed",
-        "value": "23507"
-    },
-    {
-        "label": "Thu",
-        "value": "9110"
-    },
-    {
-        "label": "Fri",
-        "value": "15529"
-    },
-    {
-        "label": "Sat",
-        "value": "20803"
-    },
-    {
-        "label": "Sun",
-        "value": "19202"
-    }];
 
-    this.companySinglePriceFusion = {
+    // first part
+    firstPriceType: string;
+    firstPriceFusion : Object;
+    firstPriceFusion_dataList:any[]=[];
+
+    // second part
+    secondPriceType: string;
+    secondPriceFusion : Object;
+    secondPriceFusion_dataList:any[]=[];
+    secondPriceFusion_catagoryList:any[]=[];
+
+  constructor(private http: HttpClient) {
+    //第一张图的数据部分
+    this.paramOfFirstChart = "500112";
+    this.firstPriceType = "line";
+    this.firstPriceFusion = {
         "chart": {
             "caption": "Single Company chart",
-            "subCaption": "Last week",
+            // "subCaption": "Last week",
             "lineThickness": "2",
             "theme": "fusion"
         },
-        "data": this.companySinglePriceFusion_data
+        "data": this.firstPriceFusion_dataList
         
     };
 
- 
+    // 第二张图的数据部分
+    this.paramOfFirstChart = "500112";
+    this.secondPriceType = "msline";
+    this.secondPriceFusion_catagoryList
+
+    this.secondPriceFusion = {
+        "chart": {
+            "caption": "Number of visitors last week",
+            "subCaption": "Bakersfield Central vs Los Angeles Topanga",
+            "xAxisName": "Day",
+            "theme": "fusion"
+        },
+        "categories": [
+            {
+                "category": this.secondPriceFusion_catagoryList
+                // [
+                //     {
+                //         "label": "Mon"
+                //     },
+                //     {
+                //         "label": "Tue"
+                //     },
+                //     {
+                //         "label": "Wed"
+                //     },
+                //     {
+                //         "vline": "true",
+                //         "lineposition": "0",
+                //         "color": "#6baa01",
+                //         "labelHAlign": "center",
+                //         "labelPosition": "0",
+                //         "label": "National holiday",
+                //         "dashed": "1"
+                //     },
+                //     {
+                //         "label": "Thu"
+                //     },
+                //     {
+                //         "label": "Fri"
+                //     },
+                //     {
+                //         "label": "Sat"
+                //     },
+                //     {
+                //         "label": "Sun"
+                //     }
+                // ]
+            }
+        ],
+        "dataset": [
+            {
+                "seriesname": "Bakersfield Central",
+                "data": this.secondPriceFusion_dataList
+                // [
+                //     {
+                //         "value": "15123"
+                //     },
+                //     {
+                //         "value": "14233"
+                //     },
+                //     {
+                //         "value": "25507"
+                //     },
+                //     {
+                //         "value": "9110"
+                //     },
+                //     {
+                //         "value": "15529"
+                //     },
+                //     {
+                //         "value": "20803"
+                //     },
+                //     {
+                //         "value": "19202"
+                //     }
+                // ]
+            },
+            {
+                "seriesname": "Los Angeles Topanga",
+                "data": this.secondPriceFusion_dataList
+                // [
+                //     {
+                //         "value": "13400"
+                //     },
+                //     {
+                //         "value": "12800"
+                //     },
+                //     {
+                //         "value": "22800"
+                //     },
+                //     {
+                //         "value": "12400"
+                //     },
+                //     {
+                //         "value": "15800"
+                //     },
+                //     {
+                //         "value": "19800"
+                //     },
+                //     {
+                //         "value": "21800"
+                //     }
+                // ]
+            }
+         ],
+    };
   } // end of constructor
   ngOnInit() {
-    this.http.post("http://localhost:8081/search/comparison/"+ this.companyName,{start:'2019-06-08 10:30:00',end:'2019-06-09 10:30:00'} ).subscribe(res=>{
-        this.setData(res)});
-  }
-
-  setData(returnedObj: any){
-    returnedObj.data.forEach(element => {
-        console.log("element: ", element.currentPrice, ", datetime:", element.dateTime);
-        
-        this.arraytemp.push[this.arraytempStr];
-        console.log("arraytemp: ", this.arraytemp);
-    });
     
   }
+
+  getDatas() {
+    this.getFirstChartData();
+    this.getSecondChartData();
+  }
+
+  // Single company chart 
+  getFirstChartData(){
+    this.http.post("http://localhost:8081/search/comparison/"+ this.paramOfFirstChart,{start:'2019-06-08 10:30:00',end:'2019-06-09 10:30:00'} ).subscribe(res=>{
+    this.getFirstData(res)});
+  }
+  getFirstData(returnedObj: any){
+      console.log("res: " , returnedObj)
+        returnedObj.data.forEach(element => {
+            // 遍历返回的data数组, 按fusion的格式封装值
+            this.firstPriceFusion_dataList.push({
+                "label": element.dateTime,
+                "value": element.currentPrice
+            })
+        });
+  }
+
+  getSecondChartData(){
+    this.http.post("http://localhost:8081/search/comparison/companyAnd/Catering",{start:'2019-06-08 10:30:00',end:'2019-06-09 10:30:00'} ).subscribe(res=>{
+    this.getSecondData(res)
+
+    });
+  }
+  getSecondData(returnedObj: any){
+      console.log("res: " , returnedObj)
+        returnedObj.data.forEach(element => {
+            // 遍历返回的data数组, 按fusion的格式封装值
+            this.secondPriceFusion_catagoryList.push({
+                "label": element.lable,
+            })
+
+            this.secondPriceFusion_dataList.push({
+                "value": element.value
+            })
+        });
+  }
+
+
+
 
 
 
